8f1eccb7fe36b1f8a66dcfcb38ecab737bd6b88a
Daniel Foehn <dfoehn@thermoplan.ch>
2019-08-06 08:38:57 +0200

    update aliases

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -261,7 +261,7 @@ if ! zstyle -t ':prezto:module:git:alias' skip 'yes'; then
   alias gtv='git verify-tag'
 
   # Working Copy (w)
-  alias gws='git status --ignore-submodules=${_git_status_ignore_submodules} --short'
+  alias gwSs='git status --ignore-submodules=${_git_status_ignore_submodules} --short'
   alias gwS='git status --ignore-submodules=${_git_status_ignore_submodules}'
   alias gwd='git diff --no-ext-diff'
   alias gwD='git diff --no-ext-diff --word-diff'
